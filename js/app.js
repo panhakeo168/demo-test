a781f2983ee600f036bee6359b4abe216ecc5a52
panhakeo168 <panhakeo168@gmail.com>
2026-02-01 19:35:30 +0700

    update

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// exersive
+const box = document.querySelector(".box");
+document.body.onclick = function () {
+  //   if (box.classList.contains("hide")) {
+  //     box.classList.remove("hide");
+  //   } else {
+  //     box.classList.add("hide");
+  //   }
+  box.classList.toggle("hide");
+};
